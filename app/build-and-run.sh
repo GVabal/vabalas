58b08e0d7fe878db88877dd4bf8bb943c533e9a8
Gabrielius Vabalas <gvabal@gmail.com>
2024-10-28 19:22:26 +0200

    setup docker compose

--- app/build-and-run.sh
+++ app/build-and-run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+echo '  _           _ _     _                   _                    '
+echo ' | |         (_) |   | |                 | |                   '
+echo ' | |__  _   _ _| | __| |   __ _ _ __   __| |  _ __ _   _ _ __  '
+echo ' | '_ \| | | | | |/ _` |  / _` | '_ \ / _` | | '__| | | | '_ \ '
+echo ' | |_) | |_| | | | (_| | | (_| | | | | (_| | | |  | |_| | | | |'
+echo ' |_.__/ \__,_|_|_|\__,_|  \__,_|_| |_|\__,_| |_|   \__,_|_| |_|'
+echo '                                                               '
 mvn clean package && \
 docker build -t app:prod . && \
-docker-compose -f docker-compose.prod.yaml -p app up -d
+docker-compose up -d
