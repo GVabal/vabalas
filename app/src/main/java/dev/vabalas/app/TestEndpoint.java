2b464cb23dd6ea7b37de16b8c2a4a78a2618cd97
Gabrielius Vabalas <gvabal@gmail.com>
2024-11-05 23:26:54 +0200

    add frontend

--- app/src/main/java/dev/vabalas/app/TestEndpoint.java
+++ app/src/main/java/dev/vabalas/app/TestEndpoint.java
@@ -2,9 +2,11 @@ package dev.vabalas.app;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api")
 @CrossOrigin(origins = "https://vabalas.my.id/", maxAge = 3600)
 public class TestEndpoint {
 
