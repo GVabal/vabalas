00957945a20c153173106d361ad2359983f57955
Gabrielius Vabalas <gvabal@gmail.com>
2024-11-05 18:45:52 +0200

    clean app

--- build.sh
+++ build.sh
@@ -5,6 +5,16 @@ echo "▐▌ ▐▌▐▌ ▐▌  █  ▐▌   ▐▌  █"
 echo "▐▛▀▚▖▐▌ ▐▌  █  ▐▌   ▐▌  █"
 echo "▐▙▄▞▘▝▚▄▞▘▗▄█▄▖▐▙▄▄▖▐▙▄▄▀"
 echo "                         "
+
+echo "APP"
+cd app
 mvn clean package && \
 docker build -t app:prod . && \
+cd ..
+
+echo "WEB"
+cd web
+# todo
+cd ..
+
 echo "Done!"
