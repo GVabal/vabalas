608d7fa82036bfd1e4c959d7647fba2d93264e1c
Gabrielius Vabalas <gvabal@gmail.com>
2024-11-08 19:25:42 +0200

    Revert "add frontend"
    This reverts commit 1355d4c019c02188611e73f4465c3d8db68fdfd5.

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,34 +1,29 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {HttpClient} from "@angular/common/http";
+import {response} from 'express';
+import {HttpClient} from '@angular/common/http';
 
 interface IYeet {
-    yeet: string;
+  yeet: string;
 }
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    imports: [RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-    title = 'web';
 
-    constructor(private http: HttpClient) {
-    }
+  title = 'web';
 
-    ngOnInit(): void {
-        if (Date.now() % 2 == 0) {
-            console.log(Date.now());
-            this.http.get<IYeet>("https://api.vabalas.my.id/test")
-                .subscribe(response => {
-                    console.log(response);
-                    console.log(Date.now());
-                });
-        } else {
-            console.log("no http call this time");
-        }
-    }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    console.log("website")
+    this.httpClient.get<IYeet>('/api/test')
+      .subscribe(response => console.log("website", response.yeet));
+  }
 }
