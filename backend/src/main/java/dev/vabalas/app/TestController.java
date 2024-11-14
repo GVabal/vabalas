af670beaded50678a3faa200d194c395a0b513f3
Gabrielius Vabalas <gvabal@gmail.com>
2024-11-14 20:17:17 +0200

    elk

--- backend/src/main/java/dev/vabalas/app/TestController.java
+++ backend/src/main/java/dev/vabalas/app/TestController.java
@@ -1,13 +1,22 @@
 package dev.vabalas.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private static final Logger log = LoggerFactory.getLogger(TestController.class);
+
     @GetMapping
     public Yeet yeet() {
+        if (System.currentTimeMillis() % 2 == 0) {
+            log.info("something interesting");
+        } else {
+            log.error("this is concerning");
+        }
         return new Yeet("yeet");
     }
 
