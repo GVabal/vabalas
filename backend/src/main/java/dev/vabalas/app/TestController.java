f6b460aa29aa64411f1a3217c02d2f13f38a1e44
Gabrielius Vabalas <gvabal@gmail.com>
2024-11-24 17:34:47 +0200

    simple logs

--- backend/src/main/java/dev/vabalas/app/TestController.java
+++ backend/src/main/java/dev/vabalas/app/TestController.java
@@ -1,14 +1,12 @@
 package dev.vabalas.app;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
-
-    private static final Logger log = LoggerFactory.getLogger(TestController.class);
 
     @GetMapping
     public Yeet yeet() {
@@ -23,9 +21,9 @@ public class TestController {
     @GetMapping("api")
     public Yeet yeetApi() {
         if (System.currentTimeMillis() % 2 == 0) {
-            log.info("something interesting");
+            log.info("something interesting api");
         } else {
-            log.error("this is concerning");
+            log.error("this is concerning api");
         }
         return new Yeet("yeet api");
     }
